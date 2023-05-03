64415f06d39366635027e76cccf76f7f4414f95c
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-05-03 18:33:04 +0900

    2023. 5. 3. PM 6:32 [FIX] - 파일명 변경

---
+++ MC2-LESSERAFIM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MC2-LESSERAFIM 2
+//
+//  Created by Kim Andrew on 2023/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
