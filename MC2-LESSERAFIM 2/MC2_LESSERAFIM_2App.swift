dea6b929849b392c866aa68932ba2e753a2a6c84
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-05-03 17:44:34 +0900

    2023. 5. 3. PM 5:44 [ADD] - 기본 파일들 업로드

---
+++ MC2-LESSERAFIM 2/MC2_LESSERAFIM_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_LESSERAFIM_2App.swift
+//  MC2-LESSERAFIM 2
+//
+//  Created by Kim Andrew on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct MC2_LESSERAFIM_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
