bc32b33a6bda419ac53202cb9c9af0beaabbe622
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-05-06 21:33:49 +0900

    [ADD] - 캔버스 툴바 추가 - 툴바 클릭시 옵션바 나옴 - 부분 기능 완성

--- MC2-LESSERAFIM/CanvusView.swift
+++ MC2-LESSERAFIM/CanvusView.swift
@@ -23,6 +23,16 @@ struct CanvusView: View {
     @State private var selectedColor : Color = .black //기본 색상은 Black으로 지정
     @State private var thickness : Double = 1.0 //기본 팬 굵기
     
+    //Zstack visible Bool & kind
+    @State private var toolClicked = false
+    @State private var whatTool = 0
+    
+    //Challenge Title TextField
+    @State private var challengeTitle = ""
+    
+    //Challege Content TextField
+    @State private var challegeContent = ""
+    
     //View에 사용될 Canvus 정의
     var canvus : some View{
         Canvas { context, size in
@@ -48,24 +58,124 @@ struct CanvusView: View {
             )
     }
     
+    var drawingTools : some View {
+        HStack{
+            Image(systemName: "pencil.tip")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 30)
+                .gesture(TapGesture(count: 1).onEnded({
+                    toolClicked.toggle()
+                    whatTool = 1
+                }))
+            Spacer()
+            
+            Image(systemName: "eraser")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 30)
+                .gesture(TapGesture(count: 1).onEnded({
+                    toolClicked.toggle()
+                    whatTool = 2
+                }))
+            Spacer()
+            
+            Image(systemName: "scribble.variable")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 30)
+                .gesture(TapGesture(count: 1).onEnded({
+                    toolClicked.toggle()
+                    whatTool = 3
+                }))
+            Spacer()
+            
+            Image(systemName: "eyedropper")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 30)
+                .gesture(TapGesture(count: 1).onEnded({
+                    toolClicked.toggle()
+                    whatTool = 4
+                }))
+            Spacer()
+            
+            Image(systemName: "circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 30)
+                .gesture(TapGesture(count: 1).onEnded({
+                    toolClicked.toggle()
+                    whatTool = 5
+                }))
+            
+        }
+        .frame(width: 345, height: 44)
+    }
+    
     var body: some View {
         
         NavigationStack{
             //화면 시작
-            VStack{
-                
-                canvus
-                
-                }
-                .toolbar{
-                    //Tool bar에 체크 버튼 생성
-                    Button {
-                        //체크 버튼 엑션
-                        print("Button Clicked")
-                    } label: {
-                        Image(systemName: "checkmark.circle")
+            ZStack {
+                VStack{
+                    
+                    canvus
+                    
+                    drawingTools
+                    
+                    TextField("이번 챌린지 사진에 제목을 붙여볼까요?", text: $challengeTitle)
+                        .padding()
+                        .frame(width: 345, height: 39)
+                        .border(Color.black)
+                    
+                    TextField("어떤 이야기가 담겨있나요?", text: $challegeContent)
+                        .padding()
+                        .frame(width: 345, height: 122)
+                        .border(Color.black)
+                    
                     }
+                    .toolbar{
+                        //Tool bar에 체크 버튼 생성
+                        Button {
+                            //체크 버튼 엑션
+                            print("Button Clicked")
+                        } label: {
+                            Image(systemName: "checkmark.circle")
+                        }
                 }
+                
+                VStack{ //case문 좀 치겠는데?
+                    Spacer()
+                        .frame(width: 325, height: 523)
+                    
+                    if toolClicked {
+                        switch whatTool {
+                        case 1:
+                            Slider(value: $thickness, in: 1...30)
+                        
+                        case 2:
+                            Slider(value: $thickness, in: 1...30)
+                            
+                        case 3:
+                            CanvusColorPickerView(selectedColor: $selectedColor)
+                                .onChange(of: selectedColor){ changeColor in
+                                    currentLine.color = changeColor
+                                }
+                        case 4:
+                            Slider(value: $thickness, in: 1...30)
+                            
+                        case 5:
+                            Slider(value: $thickness, in: 1...30)
+                            
+                        default:
+                            Spacer()
+                            
+                        }
+                    }
+                    
+                }
+            }
         }
     }
 }
