4296381c07314aad6750eb91dfd45d658a658734
sohnseoyeon <s.seoyeon621@gmail.com>
2023-05-08 11:47:24 +0900

    [CHORE] 유저데이터 코드 추가

--- MC2-LESSERAFIM/ContentView.swift
+++ MC2-LESSERAFIM/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var userData = UserData()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ChallengeScreen()
+            .environmentObject(userData)
     }
 }
 
