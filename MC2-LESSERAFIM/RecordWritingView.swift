69433cf86bb208d6d2c1b89fec110fe3313f6990
ocean <funboard6287@gmail.com>
2023-05-08 12:04:22 +0900

    [CHORE]
    기록/글 화면 수정 중
    - 플레이스홀더 수정중

--- MC2-LESSERAFIM/RecordWritingView.swift
+++ MC2-LESSERAFIM/RecordWritingView.swift
@@ -9,61 +9,67 @@ import SwiftUI
 
 struct RecordWritingView: View {
     @State private var title: String = ""
+    @State private var story: String = ""
     
     var body: some View {
         ZStack {
             VStack {
                 // 챌린지 타이틀
-                VStack {
-                    TextEditor(text: $title)
+                ZStack {
+                    // 챌린지 타이틀 프레임
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 0.5)
                         .frame(width: 345, height: 39)
-                        .cornerRadius(8)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                        .onAppear {
-                            UITextView.appearance().backgroundColor = .clear
-                        }
-                        .multilineTextAlignment(.center)
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                        .padding(.horizontal)
+                    
+                    // 챌린지 타이틀 입력란
+                    VStack {
+                        TextEditor(text: $title)
+                            .frame(width: 345, height: 39)
+                            .cornerRadius(8)
+                            .onAppear {
+                                UITextView.appearance().backgroundColor = .clear
+                            }
+                            .multilineTextAlignment(.leading)
+                            .font(.system(size: 13))
+                            .foregroundColor(.gray)
+                            .padding(.horizontal)
+                    }
+                    // 챌린지 타이틀 플레이스홀더
+                    .overlay(
+                        Text(title.isEmpty ? "이번 챌린지에 제목을 붙여볼까요?" : "")
+                            .font(.system(size: 13))
+                            .frame(width: 345, alignment: .topLeading)
+                            .foregroundColor(Color(.systemGray4))
+                            .offset(x: 5, y: 11)
+                            .animation(.easeInOut(duration: 0.05))
+                        , alignment: .top
+                    )
                 }
-                .overlay(
-                    Text(title.isEmpty ? "이번 챌린지에 제목을 붙여볼까요?" : "")
-                        .font(.headline)
-                        .frame(width: 345, alignment: .topLeading)
-                        .foregroundColor(Color(.systemGray4))
-                        .offset(x: 0, y: title.isEmpty ? -10 : 10)
-                        .animation(.easeInOut(duration: 0.2))
-                    , alignment: .top
-                )
                 
                 // 챌린지 내용
                 VStack {
-                    TextEditor(text: $title)
+                    TextEditor(text: $story)
                         .frame(width: 345, height: 561)
                         .cornerRadius(8)
                         .overlay(
                             RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.gray, lineWidth: 1)
+                                .stroke(Color.gray, lineWidth: 0.5)
                         )
                         .onAppear {
                             UITextView.appearance().backgroundColor = .clear
                         }
-                        .multilineTextAlignment(.center)
-                        .font(.headline)
+                        .multilineTextAlignment(.leading)
+                        .font(.system(size: 13))
                         .foregroundColor(.gray)
                         .padding(.horizontal)
                 }
                 .overlay(
-                    Text(title.isEmpty ? "어떤 이야기가 담겨있나요?" : "")
-                        .font(.headline)
+                    Text(story.isEmpty ? "어떤 이야기가 담겨있나요?" : "")
+                        .font(.system(size: 13))
                         .frame(width: 345, alignment: .topLeading)
                         .foregroundColor(Color(.systemGray4))
-                        .offset(x: 0, y: title.isEmpty ? -10 : 10)
-                        .animation(.easeInOut(duration: 0.2))
+                        .offset(x: 5, y: 11)
+                        .animation(.easeInOut(duration: 0.05))
                     , alignment: .top
                 )
             }
