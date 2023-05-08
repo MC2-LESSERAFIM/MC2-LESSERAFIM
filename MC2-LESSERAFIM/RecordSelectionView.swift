d97263c56089927159d563a147078e7fbff83e32
ocean <funboard6287@gmail.com>
2023-05-08 10:39:52 +0900

    [ADD]
    기록 화면 생성
    - NavigationLink 기능 제외
    - 타이틀, 버튼 생성

---
+++ MC2-LESSERAFIM/RecordSelectionView.swift
@@ -0,0 +1,58 @@
+//
+//  RecordSelectionView.swift
+//  MC2-LESSERAFIM
+//
+//  Created by OhSuhyun on 2023/05/08.
+//
+
+import SwiftUI
+
+struct RecordSelectionView: View {
+    
+    var body: some View {
+        ZStack{
+            VStack {    // 내비게이션 백버튼 - 타이틀 - 기록선택버튼
+                // 내비게이션 백버튼
+                
+                // 화면 타이틀(PageTitle)
+                PageTitle(titlePage: "챌린지를 기록해보아요")
+                
+                // 기록선택버튼(RecordButton)
+                HStack {
+                    // 사진 버튼
+                    RecordButton(
+                        labelTitle: "사진 + 글",
+                        labelImage: "camera.fill@5x",
+                        destination: ContentView()
+                    )
+                    
+                    // 글 버튼
+                    RecordButton(
+                        labelTitle: "글",
+                        labelImage: "square.and.pencil@5x",
+                        destination: ContentView()
+                    )
+                    .padding(.horizontal, 12)   // 버튼 간의 갭
+                    
+                    // 그림 버튼
+                    RecordButton(
+                        labelTitle: "그림",
+                        labelImage: "paintpalette.fill@5x",
+                        destination: ContentView()
+                    )
+                }
+                .padding(.top, 48)  // 타이틀과의 갭 영역
+                .padding(.horizontal, 24)   // 양 옆 가드 영역
+                
+                // 위로 밀기
+                Spacer()
+            }
+        }
+    }
+}
+
+struct RecordSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordSelectionView()
+    }
+}
