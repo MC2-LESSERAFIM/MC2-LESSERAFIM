5351ff06906a3f0ec0029eb87eedd92a99747867
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-05-08 15:17:34 +0900

    [Fix] 오타 수정

--- MC2-LESSERAFIM/WritingView.swift
+++ MC2-LESSERAFIM/WritingView.swift
@@ -57,86 +57,82 @@ struct WritingView: View {
     var type: String
     
     @FocusState private var focusedField: FocusField?
-    
+
     func loadImage() {
         guard let selectedImage = selectedImage else { return }
         profileImage = Image(uiImage: selectedImage)
     }
     
     var body: some View {
-        ScrollView{
-            GeometryReader { geo in
-                VStack(spacing: 10) {
-                    
-                    Button(action: {
-                        imagePickerPresented.toggle()
-                    }, label: {
-                        if profileImage == nil {
-                            Rectangle()
-                                .foregroundColor(.gray)
-                            
-                        }
-                        else{
-                            profileImage!
-                                .resizable()
-                                .scaledToFill()
-                            self.frame.
-                            UIView.frame.
-                                .frame(width: geo.size.width - 40, height: geo.size.height - 170, alignment: .center)
-                                .clipped()
-                        }
-                    })
-                    
-                    TextField("이번 챌린지 사진에 제목을 붙여볼까요?", text: $title)
-                        .focused($focusedField, equals: .title)
-                        .submitLabel(.next)
-                    
-                    TextField("어떤 이야기가 담겨있나요?\n", text: $content, axis: .vertical)
-                        .focused($focusedField, equals: .content)
-                        .lineLimit(3...)
-                        .submitLabel(.return)
-                        .toolbar {
-                            ToolbarItemGroup(placement: .keyboard) {
-                                Button("완료") {
-                                    hideKeyboard()
-                                }
+        GeometryReader { geo in
+            VStack(spacing: 10) {
+                
+                Button(action: {
+                    imagePickerPresented.toggle()
+                }, label: {
+                    if profileImage == nil {
+                        Rectangle()
+                            .foregroundColor(.gray)
+                        
+                    }
+                    else{
+                        profileImage!
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 170, alignment: .center)
+                            .clipped()
+                    }
+                })
+                
+                TextField("이번 챌린지 사진에 제목을 붙여볼까요?", text: $title)
+                    .focused($focusedField, equals: .title)
+                    .submitLabel(.next)
+                
+                TextField("어떤 이야기가 담겨있나요?\n", text: $content, axis: .vertical)
+                    .focused($focusedField, equals: .content)
+                    .lineLimit(3...)
+                    .submitLabel(.return)
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Button("완료") {
+                                hideKeyboard()
                             }
                         }
-                }
-                .font(.subheadline)
-                .textFieldStyle(.roundedBorder)
-                .onSubmit {
-                    switch focusedField {
-                    case .title:
-                        focusedField = .content
-                    default:
-                        hideKeyboard()
                     }
+            }
+            .font(.subheadline)
+            .textFieldStyle(.roundedBorder)
+            .onSubmit {
+                switch focusedField {
+                case .title:
+                    focusedField = .content
+                default:
+                    hideKeyboard()
                 }
-                .padding(EdgeInsets(top: 47-30, leading: 20, bottom: 34, trailing: 20))
-                .alert("이미지를 업로드 해주세요", isPresented: $showingAlert) {
-                    Button("OK", role: .cancel) {
-                        self.showingAlert = false
-                    }
+            }
+            .padding(EdgeInsets(top: 47-30, leading: 20, bottom: 34, trailing: 20))
+            .alert("이미지를 업로드 해주세요", isPresented: $showingAlert) {
+              Button("OK", role: .cancel) {
+                  self.showingAlert = false
+              }
+            }
+            .sheet(isPresented: $imagePickerPresented,
+                   onDismiss: loadImage,
+                   content: { ImagePicker(image: $selectedImage) })
+            .toolbar {
+                ToolbarItem {
+                    Image(systemName: "checkmark.circle")
+                        .onTapGesture {
+                            if (selectedImage != nil) {
+                                postData.posts.append(Post(type: type, image:profileImage!, title:title, content:content))
+                                self.presentationMode.wrappedValue.dismiss()
+                            }
+                            else{
+                                self.showingAlert = true
+                            }
+                        }.foregroundColor(.blue)
                 }
-                .sheet(isPresented: $imagePickerPresented,
-                       onDismiss: loadImage,
-                       content: { ImagePicker(image: $selectedImage) })
-                .toolbar {
-                    ToolbarItem {
-                        Image(systemName: "checkmark.circle")
-                            .onTapGesture {
-                                if (selectedImage != nil) {
-                                    postData.posts.append(Post(type: type, image:profileImage!, title:title, content:content))
-                                    self.presentationMode.wrappedValue.dismiss()
-                                }
-                                else{
-                                    self.showingAlert = true
-                                }
-                            }.foregroundColor(.blue)
-                    }
-                    
-                }
+                
             }
         }
     }
