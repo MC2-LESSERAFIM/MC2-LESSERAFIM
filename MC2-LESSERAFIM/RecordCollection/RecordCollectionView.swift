b2d85462c6c83e8496d38ecabf631dad562f76d2
Damagucci-Juice <jsr9874@gmail.com>
2023-05-09 12:20:11 +0900

    [FIX] Navigation Title이 .inline, .large 두 곳에 적용되는 버그 수정
    - NavigtaionView { } 가 현재 뷰를 호출하는 곳에서 부터 중복되어 생기는 버그

--- MC2-LESSERAFIM/RecordCollection/RecordCollectionView.swift
+++ MC2-LESSERAFIM/RecordCollection/RecordCollectionView.swift
@@ -27,69 +27,29 @@ struct RecordCollectionView: View {
     
     var body: some View {
         //TODO: - 들여쓰기 레벨 줄이기
-        NavigationView {
-            GeometryReader { geo in
-                ScrollView {
-                    // MARK: - sorting by .day
-                    if selectedSort == .day {
+        GeometryReader { geo in
+            ScrollView {
+                // MARK: - sorting by .day
+                if selectedSort == .day {
+                    
+                    if postData.posts.isEmpty == false {
                         
-                        if postData.posts.isEmpty == false {
-                            
-                            VStack(alignment: .leading) {
-                                ForEach(0..<5) { i in
-                                    HStack {
-                                        ForEach(0..<3) { j in
-                                            if (i*3 + j) < postData.posts.count {
-                                                postData.posts[i*3 + j].image
-                                                    .resizable()
-                                                    .scaledToFill()
-                                                    .frame(width: (geo.size.width - 8) / 3, height: geo.size.width / 2.2, alignment: .center)
-                                                    .clipped()
-                                            }
+                        VStack(alignment: .leading) {
+                            ForEach(0..<5) { i in
+                                HStack {
+                                    ForEach(0..<3) { j in
+                                        if (i*3 + j) < postData.posts.count {
+                                            postData.posts[i*3 + j].image
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: (geo.size.width - 8) / 3, height: geo.size.width / 2.2, alignment: .center)
+                                                .clipped()
                                         }
                                     }
                                 }
                             }
-                            .toolbar {
-                                Picker("", selection: $selectedSort) {
-                                    ForEach(SortBy.allCases) { sort in
-                                        Label(sort.rawValue, systemImage: "arrowtriangle.down.fill")
-                                            .labelStyle(.titleAndIcon)
-                                    }
-                                }
-                            }
-                        } else {
-                            Text("No Data").toolbar {
-                                Picker("", selection: $selectedSort) {
-                                    ForEach(SortBy.allCases) { sort in
-                                        Label(sort.rawValue, systemImage: "arrowtriangle.down.fill")
-                                            .labelStyle(.titleAndIcon)
-                                    }
-                                }
-                            }
                         }
-                        
-                        // MARK: - sorting by .category
-                    } else if selectedSort == .category {
-                        LazyVGrid(columns: numberColumns, spacing: 20) {
-                            ForEach(categories) { category in
-                                NavigationLink {
-                                    Text(category.rawValue)
-                                } label: {
-                                    VStack(alignment: .leading) {
-                                        Rectangle()
-                                            .frame(width: 170, height: 170)
-                                            .foregroundColor(Color(.systemGray5))
-                                            .cornerRadius(30)
-                                        
-                                        Text(category.rawValue)
-                                            .foregroundColor(.black)
-                                            .font(.system(size: 17, weight: .medium, design: .rounded))
-                                            .padding(.leading)
-                                    }
-                                }
-                            }
-                        }.toolbar {
+                        .toolbar {
                             Picker("", selection: $selectedSort) {
                                 ForEach(SortBy.allCases) { sort in
                                     Label(sort.rawValue, systemImage: "arrowtriangle.down.fill")
@@ -97,9 +57,50 @@ struct RecordCollectionView: View {
                                 }
                             }
                         }
-
+                    } else {
+                        Text("No Data").toolbar {
+                            Picker("", selection: $selectedSort) {
+                                ForEach(SortBy.allCases) { sort in
+                                    Label(sort.rawValue, systemImage: "arrowtriangle.down.fill")
+                                        .labelStyle(.titleAndIcon)
+                                }
+                            }
+                        }
                     }
                     
+                    // MARK: - sorting by .category
+                } else if selectedSort == .category {
+                    LazyVGrid(columns: numberColumns, spacing: 20) {
+                        ForEach(categories) { category in
+                            NavigationLink {
+                                Text(category.rawValue)
+                            } label: {
+                                VStack(alignment: .leading) {
+                                    Rectangle()
+                                        .frame(width: 170, height: 170)
+                                        .foregroundColor(Color(.systemGray5))
+                                        .cornerRadius(30)
+                                    
+                                    Text(category.rawValue)
+                                        .foregroundColor(.black)
+                                        .font(.system(size: 17, weight: .medium, design: .rounded))
+                                        .padding(.leading)
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                
+            }
+            .navigationTitle("나의 기록")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                Picker("", selection: $selectedSort) {
+                    ForEach(SortBy.allCases) { sort in
+                        Label(sort.rawValue, systemImage: "arrowtriangle.down.fill")
+                            .labelStyle(.titleAndIcon)
+                    }
                 }
             }
         }
