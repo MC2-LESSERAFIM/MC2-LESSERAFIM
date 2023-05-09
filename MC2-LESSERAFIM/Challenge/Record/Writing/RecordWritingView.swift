ac312a56764fb7672682f0dcc3c1839f65510a69
ocean <funboard6287@gmail.com>
2023-05-09 11:42:28 +0900

    [CHORE]
    - RecordWritingView 수정 중
      - 챌린지 타이틀에서 플레이스 홀더 텍스트가 있는 텍스트 위치 선택이 안됨

--- MC2-LESSERAFIM/Challenge/Record/Writing/RecordWritingView.swift
+++ MC2-LESSERAFIM/Challenge/Record/Writing/RecordWritingView.swift
@@ -8,31 +8,25 @@
 import SwiftUI
 
 struct RecordWritingView: View {
-    @State private var title: String = ""
-    @State private var story: String = ""
+    @State private var title: String = ""   // 챌린지 타이틀
+    @State private var story: String = ""   // 챌린지 내용
     
     var body: some View {
         ZStack {
             VStack {
                 // 챌린지 타이틀
                 ZStack {
-                    // 챌린지 타이틀 프레임
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.gray, lineWidth: 0.5)
-                        .frame(width: 345, height: 39)
                     
                     // 챌린지 타이틀 입력란
                     VStack {
-                        TextEditor(text: $title)
-                            .frame(width: 345, height: 39)
-                            .cornerRadius(8)
-                            .onAppear {
-                                UITextView.appearance().backgroundColor = .clear
-                            }
-                            .multilineTextAlignment(.leading)
-                            .font(.system(size: 13))
-                            .foregroundColor(.gray)
-                            .padding(.horizontal)
+                            /*
+                             TextEditor(text: $title)
+                             .frame(width: 345, height: 39)
+                             .cornerRadius(5)
+                             .multilineTextAlignment(.leading)
+                             .font(.system(size: 13))
+                             .offset(x: 0, y: 3) // 위치 조정
+                             */
                     }
                     // 챌린지 타이틀 플레이스홀더
                     .overlay(
@@ -40,38 +34,50 @@ struct RecordWritingView: View {
                             .font(.system(size: 13))
                             .frame(width: 345, alignment: .topLeading)
                             .foregroundColor(Color(.systemGray4))
+                            .offset(x: 5)    // 위치 조정
+                            .animation(.easeInOut(duration: 0.05))
+//                        , alignment: .top
+                    )
+                    
+                    // 챌린지 타이틀 프레임
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 0.5)
+                        .frame(width: 345, height: 39)
+                }
+                
+                // 챌린지 내용
+                ZStack {
+                    
+                    // 챌린지 타이틀 입력란
+                    VStack {
+                        TextEditor(text: $story)
+                            .frame(width: 345, height: 561)
+                            .cornerRadius(5)
+                            .onAppear {
+                                UITextView.appearance().backgroundColor = .clear
+                            }
+                            .multilineTextAlignment(.leading)
+                            .font(.system(size: 13))
+                            .offset(x: 0, y: 3)
+                            .padding(.horizontal)
+                    }
+                    // 챌린지 타이틀 플레이스홀더
+                    .overlay(
+                        Text(story.isEmpty ? "어떤 이야기가 담겨있나요?" : "")
+                            .font(.system(size: 13))
+                            .frame(width: 345, alignment: .topLeading)
+                            .foregroundColor(Color(.systemGray4))
                             .offset(x: 5, y: 11)
                             .animation(.easeInOut(duration: 0.05))
                         , alignment: .top
                     )
-                }
-                
-                // 챌린지 내용
-                VStack {
-                    TextEditor(text: $story)
+                    
+                    // 챌린지 타이틀 프레임
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 0.5)
                         .frame(width: 345, height: 561)
-                        .cornerRadius(8)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.gray, lineWidth: 0.5)
-                        )
-                        .onAppear {
-                            UITextView.appearance().backgroundColor = .clear
-                        }
-                        .multilineTextAlignment(.leading)
-                        .font(.system(size: 13))
-                        .foregroundColor(.gray)
-                        .padding(.horizontal)
                 }
-                .overlay(
-                    Text(story.isEmpty ? "어떤 이야기가 담겨있나요?" : "")
-                        .font(.system(size: 13))
-                        .frame(width: 345, alignment: .topLeading)
-                        .foregroundColor(Color(.systemGray4))
-                        .offset(x: 5, y: 11)
-                        .animation(.easeInOut(duration: 0.05))
-                    , alignment: .top
-                )
+                .padding(.top, 11)
             }
             .padding(.horizontal, 24)   // 양 옆 가드 영역
             
