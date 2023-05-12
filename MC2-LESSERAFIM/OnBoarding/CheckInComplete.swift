c8fff87198e9a5f4f571c0c6832f8addf9bc7f1d
sohnseoyeon <s.seoyeon621@gmail.com>
2023-05-13 02:12:05 +0900

    [CHORE] stack 정렬 및 height 및 폰트 관련 사항 수정

--- MC2-LESSERAFIM/OnBoarding/CheckInComplete.swift
+++ MC2-LESSERAFIM/OnBoarding/CheckInComplete.swift
@@ -14,22 +14,25 @@ struct CheckInComplete: View {
     @Binding var username: String
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0){
-            PageTitle(titlePage: "이제부터 당신의 짝꿍은")
-            HStack {
-                Text(username)
-                    .foregroundColor(.blue)
-                    .font(.system(size: 32, weight: .bold))
-                PageTitle(titlePage: "입니다.")
-                Spacer()
-            }
+        VStack(spacing: 0){
+                PageTitle(titlePage: "이제부터 당신의 짝꿍은")
+                HStack {
+                    Text(username)
+                        .foregroundColor(.mainPink)
+                        .font(.system(size: 32, weight: .bold))
+                    PageTitle(titlePage: "입니다.")
+                    Spacer()
+                }
+
+            Spacer()
             
             Image(tappedImageName)
                 .resizable()
                 .scaledToFit()
-                .frame(height: 400)
+                .frame(height: 500)
                 .padding(.top, 48)
-            //                    .padding(.bottom, 164)
+            
+            Spacer()
             
             NavigationLink(
                 destination: ChallengeScreen(tappedImageName: $tappedImageName, username: $username)
@@ -37,16 +40,20 @@ struct CheckInComplete: View {
                 isActive: $isStartButtonEnabled,
                 label: {
                     Button("시작하기", action: finishOnboarding)
-                        .font(.custom("ButtonStyle", size: 18))
+                        .font(.system(size: 17, weight: .bold))
                         .foregroundColor(.white)
                         .frame(width: 345,height: 50)
                         .background(
                             RoundedRectangle(cornerRadius: 12)
-                                .foregroundColor(.blue)
+                                .foregroundColor(.mainPink)
                         )
                 }
             )
+            Spacer()
         }
+        .padding(.horizontal, 24)
+        .padding(.top, 100)
+        .ignoresSafeArea()
     }
 }
 struct CheckInComplete_Previews: PreviewProvider {
