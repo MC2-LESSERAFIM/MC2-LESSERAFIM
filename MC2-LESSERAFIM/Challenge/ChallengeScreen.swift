60ff1bd9b5d61aa30e21dcff91fe5430246c2660
ocean <funboard6287@gmail.com>
2023-05-12 15:53:30 +0900

    [CHORE] ChallengeScreen 코드 정리
    - 불필요한 주석 제거
    - print문 제거
    - 코드 정리

--- MC2-LESSERAFIM/Challenge/ChallengeScreen.swift
+++ MC2-LESSERAFIM/Challenge/ChallengeScreen.swift
@@ -21,11 +21,7 @@ struct ChallengeScreen: View {
     
     private let challengeNumber: Int = 3
     
-    // [ocean ~] 앱 당일 처음 실행 여부 판독기
-    // 참고: https://paulyoungsuklee.com/2021/11/09/how-to-detect-first-time-app-launch-in-swiftui/
-    @AppStorage("dailyFirstUse") var dailyFirstUse: Bool = false    // '오늘의 챌린지 뽑기'-> true, 기록 남기기-> false
-    // print(UserDefaults.standard.bool(forKey: "dailyFirstUse"))  // 사용방법
-    // [~ ocean]
+    @AppStorage("dailyFirstUse") var dailyFirstUse: Bool = false    // 오늘 앱 처음 사용 여부 == 첫 기록 확인용
     
     var body: some View {
         NavigationView {
@@ -98,21 +94,9 @@ struct ChallengeScreen: View {
                 } else {
                     // 뽑기 버튼
                     Button {
-//                        self.isPickedChallenge = !self.isPickedChallenge // true 대신에 !self.isPickedChallenge로 사용 가능함.
                         isPickedChallenge.toggle()
-                        print("뽑기")
                         
-                        // [ocean ~]
                         dailyFirstUse = true  // 당일 챌린지 도전 0인 상태로 변경
-                        /*
-                         기록 뷰에서 기록을 저장할 때 firstCheck: Bool 값을 생성
-                         firstCheck = dailyFirstUse로 그날의 처음 여부 확인
-                         기록 뷰에서 체크 버튼을 누를 경우 false로 변경(토글 아님)
-                         false로 변경되었기 때문에 당일 다른 기록을 남길 경우 firstCheck = false인 상태
-                         (firstCheck = true)인 기록의 수 + 1 = 다음날 Day 값
-                         Today's First Launch 주석 아래 부분에 Day 값 변경 부분 작성
-                        */
-                        // [~ ocean]
                     } label: {
                         Text("오늘의 챌린지 뽑기")
                             .foregroundColor(.white)
@@ -125,38 +109,14 @@ struct ChallengeScreen: View {
                 Spacer()
             }
             .onAppear(perform: {
-                // [ocean ~] DateFormatter 사용한 '오늘의 챌린지 뽑기'로 리프레시
-                let today = NSDate().formatted
-                if (UserDefaults.standard.string(forKey: Constants.FIRSTLAUNCH) == today)
+                // DateFormatter 사용한 '오늘의 챌린지 뽑기'로 리프레시
+                let today = NSDate().formatted  // 오늘 날짜
+                let lastLaunchDate = UserDefaults.standard.string(forKey: Constants.FIRSTLAUNCH)    // 최근 사용 기록 날짜
+                if lastLaunchDate != today   // 오늘 앱 첫 실행
                     {
-                         //Already Launched today
-                    }
-                    else
-                    {
-                         //Today's First Launch
                         UserDefaults.standard.setValue(today, forKey:Constants.FIRSTLAUNCH)
                         isPickedChallenge = false   // '오늘의 챌린지' 아직 뽑지 않은 상태로 변경
                     }
-                // [~ ocean]
-                
-                /* 미확인 -> let으로 선언되어 있어 제대로 된 결과 안 나올 듯
-                // [ocean ~] 날짜 바뀔 경우 '오늘의 챌린지 뽑기'로 리프레시
-                // isPickedChallenge를 false로 변경
-                let savedDate = UserDefaults.standard.object(forKey: "lastDate") as? Date ?? Date.distantPast   // 저장된 날짜 = 이전 날짜
-                let currentDate = Date()    // 현재 날짜
-                let calendar = Calendar.current
-                let components = calendar.dateComponents([.day], from: savedDate, to: currentDate)  // 현재 날짜와 저장된 날짜 간의 차이 = 날짜 바뀜(Bool)
-
-                // 날짜가 바뀌었을 경우
-                if let day = components.day, day >= 1 {
-//                    // 오늘 챌린지와 삭제된 챌린지 초기화
-//                    userData.todayChallenges.removeAll()
-//                    userData.todayRemovedChallenges.removeAll()
-                    isPickedChallenge = false   // '오늘의 챌린지' 아직 뽑지 않은 상태로 변경
-                    UserDefaults.standard.set(currentDate, forKey: "lastDate")  // 현재 날짜로 저장된 날짜 변경 = 날짜 바뀜 없음
-                }
-                // [~ ocean]
-                 */
                 
                 if userData.todayChallenges.isEmpty {
                     initChallenges(number: challengeNumber)
@@ -207,17 +167,14 @@ fileprivate extension ChallengeScreen {
     }
 }
 
-// [ocean ~] [ocean ~] DateFormatter 사용한 '오늘의 챌린지 뽑기'로 리프레시
-// 참고: https://www.appsloveworld.com/swift/100/166/how-to-detect-daily-first-launch-in-ios
 extension NSDate {
     var formatted: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MM/dd/yyyy"
-        return formatter.string(from: self as Date as Date)
+        return formatter.string(from: self as Date)
     }
 }
 
 struct Constants {
     static let FIRSTLAUNCH = "first_launch"
 }
-// [~ ocean]
