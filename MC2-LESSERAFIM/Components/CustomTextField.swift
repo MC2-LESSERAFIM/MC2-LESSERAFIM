5a5b661b811709e71f7d8326fc21fc911de0cb0c
sohnseoyeon <s.seoyeon621@gmail.com>
2023-05-15 01:50:56 +0900

    [ADD] OnlyWritingContentTextField 추가
    - RecordWritingView에서만 사용

--- MC2-LESSERAFIM/Components/CustomTextField.swift
+++ MC2-LESSERAFIM/Components/CustomTextField.swift
@@ -70,3 +70,41 @@ struct TitleTextField: View {
         }
     }
 }
+
+struct OnlyWritingContentTextField: View {
+    @Binding var contentRecord: String   // 챌린지 내용
+    @State var onStory: Bool
+    
+    var placeholder: String
+    
+    var body: some View {
+        GeometryReader() { geo in
+            ScrollView {
+                ZStack {
+                    TextField("", text: $contentRecord,  axis: .vertical)
+                        .placeholder(when: contentRecord.isEmpty, placeholder: {
+                            Text("\(placeholder)")
+                                .font(.system(size: 15, weight: .regular))
+                                .foregroundColor(.mainGray)
+                        })
+                        .frame(height: onStory ? geo.size.height - 100 : 620, alignment: .top)   // 챌린지 내용 입력 중이면 키보드에 가리지 않게 크기 유동적으로 수정
+                        .lineLimit(1...)
+                        .padding(.horizontal, 12)
+                        .padding(.top, 15)
+                        .foregroundColor(.mainBlack)
+                        .font(.system(size: 15, weight: .regular))
+                        .accentColor(.mainPink)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                }
+                .padding(.horizontal, 6)
+                .background(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.white, lineWidth: 1)
+                        .background(Color.opacityWhite)
+                )
+                .cornerRadius(5)
+            }
+        }
+    }
+}
