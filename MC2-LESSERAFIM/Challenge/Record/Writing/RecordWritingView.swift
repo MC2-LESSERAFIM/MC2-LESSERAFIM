8da8abe6a5611fdc1efbd9bc80c6991fada9a02c
sohnseoyeon <s.seoyeon621@gmail.com>
2023-05-15 02:15:22 +0900

    [CHORE] RecordWritingView - TextField 컴포넌트 사용

--- MC2-LESSERAFIM/Challenge/Record/Writing/RecordWritingView.swift
+++ MC2-LESSERAFIM/Challenge/Record/Writing/RecordWritingView.swift
@@ -14,37 +14,19 @@ struct RecordWritingView: View {
     @State private var showingAlert = false // 경고 출력 여부
     @Environment(\.dismiss) private var dismiss // 화면 이탈
     
-    @State private var onStory = false   // 챌린지 내용 입력 중 여부
+    @State var onStory = false   // 챌린지 내용 입력 중 여부
     
     var body: some View {
         GeometryReader() { geo in   // 화면 크기 이용을 위해 사용
             VStack() {
-                // 챌린지 내용
-                TextField("이번 챌린지에 제목을 붙여볼까요?", text: $titleRecordWriting)
-                    .textFieldStyle(.plain)
-                    .frame(height: 39, alignment: .center)
-                    .padding(.leading, 5)
-                    .lineLimit(1)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.gray, lineWidth: 0.5)
-                    )
-                    .onTapGesture {
-                        onStory = false
-                    }
+                // 챌린지 제목
+                TitleTextField(titleRecord: $titleRecord, placeholder: "이번 챌린지에 제목을 붙여볼까요?")
+                    .submitLabel(.return)
                 
                 // 챌린지 내용
-                TextField("어떤 이야기가 담겨있나요?", text: $storyRecordWriting, axis: .vertical)
-                    .textFieldStyle(.plain)
-//                    .frame(height: geo.size.height - 100, alignment: .top)
-                    .frame(height: onStory ? geo.size.height - 100 : 620.7, alignment: .top)   // 챌린지 내용 입력 중이면 키보드에 가리지 않게 크기 유동적으로 수정
-                    .padding(.top, 11)
-                    .padding(.leading, 5)
-                    .lineLimit(onStory ? 16... : 28...)  // 키보드X: 31줄, 키보드O: 16줄
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.gray, lineWidth: 0.5)
-                    )
+                OnlyWritingContentTextField(contentRecord: $contentRecord, onStory: false, placeholder: "어떤 이야기가 담겨있나요?")
+                    .submitLabel(.return)
+                    .frame(height: onStory ? geo.size.height - 100 : 620)   // 챌린지 내용 입력 중이면 키보드에 가리지 않게 크기 유동적으로 수정
                     .onTapGesture {
                         onStory = true
                         print(geo.size.height)
@@ -67,10 +49,10 @@ struct RecordWritingView: View {
                 Image(systemName: "checkmark.circle")
                     .foregroundColor(.blue) // 기본색이 검정이어서 변경
                     .onTapGesture {
-                        if titleRecordWriting == "" {   // 제목 입력 누락
+                        if titleRecord == "" {   // 제목 입력 누락
                             print("no title")
                             self.showingAlert = true
-                        } else if storyRecordWriting == "" {    // 내용 입력 누락
+                        } else if contentRecord == "" {    // 내용 입력 누락
                             print("no story")
                             self.showingAlert = true
                         } else {    // 내용 입력 누락이 없을 경우 제출 가능
