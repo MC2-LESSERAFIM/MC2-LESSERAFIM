1c401ff1ed6ba4ac90343c3b26775b5ff9ef6a59
sohnseoyeon <s.seoyeon621@gmail.com>
2023-05-16 11:20:37 +0900

    [ADD] NextButtonStyle ButtonStyle 추가

--- MC2-LESSERAFIM/Components/CustomButton.swift
+++ MC2-LESSERAFIM/Components/CustomButton.swift
@@ -46,3 +46,21 @@ struct SelectableButton: View {
     }
 }
 
+struct NextButtonStyle: ButtonStyle {
+    var isAbled: Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 17, weight: .semibold))
+            .frame(height: 48)
+            .background(isAbled ? Color.mainPink : Color.disabledButtonGray)
+            .foregroundColor(Color.white)
+            .cornerRadius(12)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(isAbled ? Color.mainPink : Color.disabledButtonGray, lineWidth: 2)
+            }
+            .opacity(configuration.isPressed ? 0.8 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+    }
+}
