6747bf6c96991894f239022befdbd260444f6abd
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-05-16 17:41:05 +0900

    [Del] delete unused codes
    delete unused codes

--- MC2-LESSERAFIM/ContentView.swift
+++ MC2-LESSERAFIM/ContentView.swift
@@ -7,57 +7,6 @@
 
 import SwiftUI
 
-struct PostModel : Identifiable, Codable {
-    var id = UUID()
-    var type: String
-    var title: String
-    var content: String
-    var category: Category
-    var image: Image {
-        Image.fromData(imageData ?? Data()) ?? Image(systemName: "x.circle")
-    }
-    private var imageData: Data?
-    
-    // MARK: - init 1
-    init(type: String, imageData: Data?, title: String, content: String, category: Category) {
-        self.type = type
-        self.imageData = imageData
-        self.title = title
-        self.content = content
-        self.category = category
-    }
-    
-    // MARK: - init 2, Decode
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        id = try container.decode(UUID.self, forKey: .id)
-        type = try container.decode(String.self, forKey: .type)
-        title = try container.decode(String.self, forKey: .title)
-        content = try container.decode(String.self, forKey: .content)
-        category = try container.decode(Category.self, forKey: .category)
-
-        imageData = try container.decode(Data.self, forKey: .imageData)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(type, forKey: .type)
-        try container.encode(imageData, forKey: .imageData)
-        try container.encode(title, forKey: .title)
-        try container.encode(content, forKey: .content)
-        try container.encode(category, forKey: .category)
-    }
-    
-    enum CodingKeys : String, CodingKey {
-        case id
-        case type, content, title
-        case imageData
-        case category
-    }
-}
-
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) private var viewContext
